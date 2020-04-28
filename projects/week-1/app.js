d0390730d067002d1b958bc9d03d264a9dadb644
Unknown <craigdennis1990@gmail.com>
2020-04-28 12:27:18 +0100

    retrieving data from API

--- projects/week-1/app.js
+++ projects/week-1/app.js
@@ -1,1 +1,27 @@
-alert("this is a test");
+//if user enters a space, replace with dash
+
+
+
+const searchButton = document.getElementById('search');
+const input = document.getElementById('users-input');
+
+searchButton.addEventListener('click', function () {
+  pokemonRequest(input.value);
+});
+
+function pokemonRequest(name) {
+  fetch('https://pokeapi.co/api/v2/pokemon/' + name)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data.name);
+      console.log(data.types[0].type.name);
+      console.log(data.abilities);
+      console.log(data.sprites.front_default);
+
+    })
+    .catch((err) => {
+        //do something with error
+    });
+}
