2a33d02197f00fdde56821b07bf49f3568a60799
Unknown <craigdennis1990@gmail.com>
2020-04-28 15:16:13 +0100

    api query allows spaces
    replaces spaces with dashes

--- projects/week-1/app.js
+++ projects/week-1/app.js
@@ -1,12 +1,12 @@
 //if user enters a space, replace with dash
 
-
-
 const searchButton = document.getElementById('search');
 const input = document.getElementById('users-input');
 
 searchButton.addEventListener('click', function () {
-  pokemonRequest(input.value);
+    var pokemonName = String(input.value);
+    var formattedPokemonName = pokemonName.replace(" ", "-");
+  pokemonRequest(formattedPokemonName);
 });
 
 function pokemonRequest(name) {
@@ -15,13 +15,13 @@ function pokemonRequest(name) {
       return response.json();
     })
     .then((data) => {
-      console.log(data.name);
-      console.log(data.types[0].type.name);
-      console.log(data.abilities);
-      console.log(data.sprites.front_default);
-
+        document.getElementById('name').textContent = data.name;
+        document.getElementById('type').textContent = data.types[0].type.name;
+        document.getElementById('img').src = data.sprites.front_default;
+        console.log(data.abilities);
     })
     .catch((err) => {
-        //do something with error
+      //do something with error
+      alert("The pokemon you have entered is not recognised. Please try again")
     });
 }
