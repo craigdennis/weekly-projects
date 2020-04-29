e2eb7b0d45ec806b88026cd44c05f79a95f835f9
Unknown <craigdennis1990@gmail.com>
2020-04-29 12:32:02 +0100

    added audio and styling

--- projects/week-1/app.js
+++ projects/week-1/app.js
@@ -4,9 +4,9 @@ const searchButton = document.getElementById('search');
 const input = document.getElementById('users-input');
 
 searchButton.addEventListener('click', function () {
-    var pokemonName = String(input.value);
-    var formattedPokemonName = pokemonName.replace(" ", "-");
-  pokemonRequest("ditto");
+  var pokemonName = String(input.value);
+  var formattedPokemonName = pokemonName.replace(' ', '-');
+  pokemonRequest(formattedPokemonName);
 });
 
 function pokemonRequest(name) {
@@ -15,22 +15,31 @@ function pokemonRequest(name) {
       return response.json();
     })
     .then((data) => {
-        let abilityString = "";
+      let abilityString = 'Abilities: ';
 
-        document.getElementById('name').textContent = data.name;
-        document.getElementById('type').textContent = data.types[0].type.name;
-        document.getElementById('img').src = data.sprites.front_default;
-        let abilities = data.abilities
-        
-        for (const ability of abilities) {
-           abilityString += ability.ability.name
-           abilityString += " ";
-            console.log(ability.ability.name);
-        }
-        document.getElementById('ability').textContent = abilityString;
+      document.getElementById('name').textContent = data.name;
+      document.getElementById('type').textContent = data.types[0].type.name;
+      document.getElementById('img').src = data.sprites.front_default;
+      let abilities = data.abilities;
+
+
+
+      for (const ability of abilities) {
+          const name = ability.ability.name;
+          const nameCapitalized = name.charAt(0).toUpperCase() + name.slice(1)
+
+        abilityString += nameCapitalized;
+        abilityString += " ";
+
+        console.log(ability.ability.name);
+      }
+      document.getElementById('ability').textContent = abilityString;
+
+      var audio = new Audio('pokemon-clip.mp3');
+      audio.play();
     })
     .catch((err) => {
       //do something with error
-      alert("The pokemon you have entered is not recognised. Please try again")
+      alert('The pokemon you have entered is not recognised. Please try again');
     });
 }
